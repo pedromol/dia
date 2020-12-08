33091d0cb31316ebc632a0dfed233fb99b92bd1e
eder.costa <eder.costa@bemobi.com.br>
2020-12-08 16:07:56 -0300

    Add a commander, with cli options

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
 const wsd = require("websequencediagrams");
 const fs = require("fs");
 const util = require("util");
+const { Command } = require("commander");
+
+const program = new Command();
+
+program.version('0.0.1');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
-
-const outputFormat = "png";
 
 const regexSplitDiagrams = /(^|\r|\n)title[ \t]+/g;
 const regexEmptyLines = /[ \t\r\n]+/g;
 const regexDiagramTitle = /^title[ \t]+([^\r\n]+)/;
 
-const inputFile = "./sequence.diagrams";
-const outputDirectory = "./output";
-
-const buildDiagram = async (diagramSource) => {
+const buildDiagram = async (diagramSource, extension = "png") => {
   return wsd
-    .diagram(diagramSource, "default", outputFormat)
+    .diagram(diagramSource, "default", extension)
     .then((result) => result[0])
-    .then((buf) => writeFile(`${outputDirectory}/${diagramSource.match(regexDiagramTitle)[1]}.${outputFormat}`, buf));
+    .then((buf) => writeFile(`./${diagramSource.match(regexDiagramTitle)[1]}.${extension}`, buf));
 };
 
-const notEmptyString = (line) => {
-  return line && line.replace(regexEmptyLines, "").length;
-};
+const notEmptyString = (line) => line && line.replace(regexEmptyLines, "").length;
 
-const addTitle = (newLine) => {
-  return `title ${newLine}`;
-};
+const addTitle = (newLine) => `title ${newLine}`;
 
-readFile(inputFile).then((diagrams) => {
-  return Promise.all(
-    diagrams
-    .toString()
-    .split(regexSplitDiagrams)
-    .filter(notEmptyString)
-    .map(addTitle)
-    .map(buildDiagram)
-  );
-});
+program
+    .command('sequence <input>')
+    .option('-e, --extension <type>', 'file extension')
+    .action((input) => {
+
+         readFile(input).then((diagrams) => {
+            return Promise.all(
+                diagrams
+                .toString()
+                .split(regexSplitDiagrams)
+                .filter(notEmptyString)
+                .map(addTitle)
+                .map((diagram) => buildDiagram(diagram, program.extension))
+            );
+        }); 
+
+    });
+
+program.parse(process.argv);
+
