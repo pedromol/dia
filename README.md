9f0e1470142b370daab76c0c88cbf2b6c8b76337
eder.costa <eder.costa@bemobi.com.br>
2020-12-21 01:18:50 -0300

    ignoring comments, change names, add a bin to eecute with npx on github, add a log error

---
+++ README.md
@@ -0,0 +1,9 @@
+# DIA
+
+A library to generate in batch diagram sequence using websequencediagrams
+
+## Prerequisites
+
+* NodeJS[node](#https://nodejs.org)
+
+## Installation
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const wsd = require("websequencediagrams");
 const fs = require("fs");
 const util = require("util");
@@ -5,7 +7,7 @@ const { Command } = require("commander");
 
 const program = new Command();
 
-program.version('0.0.1');
+program.version('0.0.2', '-v, --version', 'output the current version');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
@@ -14,32 +16,46 @@ const regexSplitDiagrams = /(^|\r|\n)title[ \t]+/g;
 const regexEmptyLines = /[ \t\r\n]+/g;
 const regexDiagramTitle = /^title[ \t]+([^\r\n]+)/;
 
-const buildDiagram = async (diagramSource, extension = "png") => {
+const extensions = ['png', 'jpeg', 'jpg'];
+
+const isExtensionValid = ext => extensions.find(e => e === ext) ? true : false;
+
+const buildDiagram = async (diagramSource, extension = "png", outputDir = "./") => {
+
+  if (!isExtensionValid(extension)) throw new Error("extension is not valid");
+
   return wsd
     .diagram(diagramSource, "default", extension)
     .then((result) => result[0])
-    .then((buf) => writeFile(`./${diagramSource.match(regexDiagramTitle)[1]}.${extension}`, buf));
+    .then((buf) => writeFile(`${outputDir}/${diagramSource.match(regexDiagramTitle)[1]}.${extension}`, buf));
 };
 
-const notEmptyString = (line) => line && line.replace(regexEmptyLines, "").length;
+const notEmptyString = line => line && line.replace(regexEmptyLines, "").length;
 
-const addTitle = (newLine) => `title ${newLine}`;
+const addTitle = line => `title ${line}`;
 
+// TODO: Add more chars to ignore if needed 
+const ignoreComments = line => !line.trimLeft().startsWith("#");
+    
 program
-    .command('sequence <input>')
+    .command('generate <input>')
     .option('-e, --extension <type>', 'file extension')
+    .option('-o, --output-dir <name>', 'output directory to save images')
     .action((input) => {
 
-         readFile(input).then((diagrams) => {
-            return Promise.all(
-                diagrams
-                .toString()
-                .split(regexSplitDiagrams)
-                .filter(notEmptyString)
-                .map(addTitle)
-                .map((diagram) => buildDiagram(diagram, program.extension))
-            );
-        }); 
+        readFile(input)
+            .then((diagrams) => {
+                return Promise.all(
+                    diagrams
+                    .toString()
+                    .split(regexSplitDiagrams)
+                    .filter(notEmptyString)
+                    .filter(ignoreComments)
+                    .map(addTitle)
+                    .map((diagram) => buildDiagram(diagram, program.extension, program.outputDir))
+                );
+            })
+            .catch(console.error); 
 
     });
 
